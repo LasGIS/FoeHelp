6420a7a947a03bda95356d5bcf374cb211e65d23
LasGIS <LasGIS>
2020-10-07 23:10:53 +0600

    add first docs

---
+++ docs/precache-manifest.064dfca170de36898ab59401cdf3ef42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f762ee7488663921df876b108a4424c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997589871cff913619a9",
+    "url": "/static/css/main.101d6a8a.chunk.css"
+  },
+  {
+    "revision": "0149b74fc78b4ec32bb1",
+    "url": "/static/js/2.7fad8206.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7fad8206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997589871cff913619a9",
+    "url": "/static/js/main.b23a50b1.chunk.js"
+  },
+  {
+    "revision": "b5f94912db43ea8f1be8",
+    "url": "/static/js/runtime-main.9e4ed491.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
