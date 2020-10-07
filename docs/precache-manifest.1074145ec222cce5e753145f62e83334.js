a4a790e29ade3d70a679d31756e10bbf372ec4e7
LasGIS <LasGIS>
2020-10-08 01:02:55 +0600

    docs update

---
+++ docs/precache-manifest.1074145ec222cce5e753145f62e83334.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe61115cff4b5294e6b810ad683a06dc",
+    "url": "index.html"
+  },
+  {
+    "revision": "da3b2fcb154d057d457f",
+    "url": "static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "e1777b463338b722029f",
+    "url": "static/css/main.99114661.chunk.css"
+  },
+  {
+    "revision": "da3b2fcb154d057d457f",
+    "url": "static/js/2.30854398.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "static/js/2.30854398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1777b463338b722029f",
+    "url": "static/js/main.03938519.chunk.js"
+  },
+  {
+    "revision": "158e6b49419e75b90d42",
+    "url": "static/js/runtime-main.fe753aac.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.064dfca170de36898ab59401cdf3ef42.js"
+  "/precache-manifest.1074145ec222cce5e753145f62e83334.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"), {
-
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
